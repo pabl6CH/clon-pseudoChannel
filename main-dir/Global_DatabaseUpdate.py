bf1ac1257adb2a473ac4d9f25e10fcca5be1e6f9
pabl6CH <127802519+pabl6CH@users.noreply.github.com>
2023-07-29 22:40:36 -0500

    Update Global_DatabaseUpdate.py
    agragando if para comprobar si es None o no (en cuyo caso le damos una duración predeterminada de 1 minuto)

--- main-dir/Global_DatabaseUpdate.py
+++ main-dir/Global_DatabaseUpdate.py
@@ -138,11 +138,24 @@ if halt != True:
             entryList['mediaID'] = "0"
             rndsql = "SELECT * FROM shows WHERE (customSectionName NOT LIKE 'playlist' AND duration BETWEEN 6000 and 999000) ORDER BY RANDOM() LIMIT 1"
             table.execute(rndsql)
+            #the_show = table.fetchone()
+            
+            #print("the_show:", the_show)  # Agrega esta línea para depurar
+            
+            #entryList['duration'] = str("1,"+str(int(the_show[4] / 60000)))
+            
+            
             the_show = table.fetchone()
+if the_show is not None:
+    entryList['duration'] = str("1," + str(int(the_show[4] / 60000)))
+else:
+    # Aquí puedes definir un valor predeterminado para 'duration' o tomar alguna otra acción.
+    entryList['duration'] = "1,0"  # Por ejemplo, duración predeterminada de 1 minuto.
+
+# Continúa con el resto de tu código...
+
             
-            print("the_show:", the_show)  # Agrega esta línea para depurar
             
-            entryList['duration'] = str("1,"+str(int(the_show[4] / 60000)))
             entryList['title'] = the_show[3]
             entryList['startTime'] = "00:00:00"
             entryList['dayOfWeek'] = "everyday"
